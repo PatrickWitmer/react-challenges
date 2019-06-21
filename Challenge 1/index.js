24438a83175eb8d52b704b97d72e43c42cd923da
WitmerDesigns <patrick@witmerdesigns.com>
2019-06-21 16:03:58 -0600

    Never stop learning

---
+++ Challenge 1/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <ul>
+    <li>Darkthrone</li>
+    <li>Amon Amarth</li>
+    <li>Belzebubs</li>
+  </ul>, 
+  document.getElementById('root')
+)
