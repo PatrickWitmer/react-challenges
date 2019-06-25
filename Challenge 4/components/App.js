516308724e8856144afdacf2dee60856c0c4c7cd
WitmerDesigns <patrick@witmerdesigns.com>
2019-06-25 13:17:11 -0600

    Winds howling

---
+++ Challenge 4/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function App() {
+    return (
+      <main>
+        <h1>Alchemy List</h1>
+        
+          
+            <input type="checkbox" id="aether" name="aether" />
+            <label htmlFor="aether">Aether</label>
+          
+            <input type="checkbox" id="alcohest" name="alcohest" />
+            <label htmlFor="alcohest">Alcohest</label>
+          
+            <input type="checkbox" id="bloodmoss" name="bloodmoss" />
+            <label htmlFor="bloodmoss">Bloodmoss</label>
+        
+        
+      </main>
+    )
+}
+
+export default App
