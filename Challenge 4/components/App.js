57fe663b7fa827a0575f454f7b155f9a0f1d67dc
WitmerDesigns <patrick@witmerdesigns.com>
2019-06-27 09:58:05 -0600

    quick formatting

--- Challenge 4/components/App.js
+++ Challenge 4/components/App.js
@@ -1,23 +1,19 @@
 import React from 'react'
 
 function App() {
-    return (
-      <main>
-        <h1>Alchemy List</h1>
-        
+  return (
+    <main>
+      <h1>Alchemy List</h1>
+          <input type="checkbox" id="aether" name="aether" />
+          <label htmlFor="aether">Aether</label>
           
-            <input type="checkbox" id="aether" name="aether" />
-            <label htmlFor="aether">Aether</label>
-          
-            <input type="checkbox" id="alcohest" name="alcohest" />
-            <label htmlFor="alcohest">Alcohest</label>
-          
-            <input type="checkbox" id="bloodmoss" name="bloodmoss" />
-            <label htmlFor="bloodmoss">Bloodmoss</label>
-        
-        
-      </main>
-    )
+          <input type="checkbox" id="alcohest" name="alcohest" />
+          <label htmlFor="alcohest">Alcohest</label>
+         
+          <input type="checkbox" id="bloodmoss" name="bloodmoss" />
+          <label htmlFor="bloodmoss">Bloodmoss</label>
+    </main>
+  )
 }
 
 export default App
