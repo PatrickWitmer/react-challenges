7b056b1df3e7390f40222826656dcb832956a5ef
WitmerDesigns <patrick@witmerdesigns.com>
2019-07-02 12:53:36 -0600

    inlined

--- challenge 7/Joke.js
+++ challenge 7/Joke.js
@@ -4,20 +4,13 @@ function Joke(props) {
 
   const question = props.joke.question;
 
-  if(question) {
   return (
     <section>
-      <p>Question: {props.joke.question}</p>
+      <p>{question ? 'Question: ' : ''}{props.joke.question}</p>
       <p>Punch Line: {props.joke.punchLine}</p>
     </section>
   )  
-  } else {
-    return (
-    <section>
-      <p>Punch Line: {props.joke.punchLine}</p>
-    </section>
-    )
-  }    
-}
+}  
+
 
 export default Joke
